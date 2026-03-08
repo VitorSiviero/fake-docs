858d94f7fcc6af885bf9b9e1674fb3450509cc91
sl4¢k01 <105024775+VitorSiviero@users.noreply.github.com>
2026-03-08 16:24:02 -0300

    Basic project structure
    - Set up basic Webpack project structure
    - Add CPF validator module
    - Add CPF generator module

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { validaCPF } from './modules/validaCpf.js'
+import { geraCpf } from './modules/geraCpf.js'
+import './css/style.css'
+
+console.log(validaCPF(""))
+//bloco para validar o cpf que o usuario digitou
+
+// bloco para gerar um cpf para o usuario
