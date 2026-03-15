b511a31b65adcaaae84976c7d6ddddb7ed0ce689
sl4¢k01 <105024775+VitorSiviero@users.noreply.github.com>
2026-03-15 17:20:02 -0300

    Update

--- src/main.js
+++ src/main.js
@@ -2,7 +2,24 @@ import { validaCPF } from './validators/validaCpf.js'
 import { geraCpf } from './generators/geraCpf.js'
 import './css/style.css'
 
-console.log(validaCPF(""))
-//bloco para validar o cpf que o usuario digitou
 
-// bloco para gerar um cpf para o usuario
+const conteudo = document.getElementById('content')
+
+document.addEventListener('click', e =>{
+    const el = e.target
+    const tag = el.tagName.toLowerCase()
+
+    if(tag === 'a'){
+        e.preventDefault()
+        carregaPagina(el)
+    }
+})
+async function carregaPagina(el){
+    const href = el.getAttribute('href')
+    const response = await fetch(href)
+
+    if(response.status !== 200) return
+
+    const html = await response.text()
+    conteudo.innerHTML = html
+}
